7466305cc2663c39cf895b2841c0f88bc18ef363
root <root@LAPTOP-I3BKQONI.localdomain>
2025-08-17 17:36:13 +0600

    Using varibles and accessing them using $

--- analyse-log.sh
+++ analyse-log.sh
@@ -1,24 +1,28 @@
-
 #!/bin/bash
+
+LOG_DIR="/home/logs"
+APP_LOG_FILE="application.log"
+SYS_LOG_FILE="system.log"
+
 echo "analysing log files"
 echo "==================="
 echo -e "\nList of the files updated in last 24 hours" 
-find . -name "*.log" -mtime -1
+find $LOG_DIR -name "*.log" -mtime -1
 
 echo -e "\nSearching Error logs in appication.log file"
-grep "ERROR" /home/logs/application.log
+grep "ERROR" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of error logs found in application.log"
-grep -c "ERROR" /home/logs/application.log
+grep -c "ERROR" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of fatal logs found in application.log"
-grep -c "FATAL" /home/logs/application.log
+grep -c "FATAL" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of fatal logs found in system.log"
-grep -c "FATAL" system.log
+grep -c "FATAL" "$SYS_LOG_FILE"
 
 echo -e "\nNumber of critical logs found in system.log"
-grep -c "CRITICAL" system.log
+grep -c "CRITICAL" "$SYS_LOG_FILE"
 
 echo -e "\nNumber of critical logs found in system.log"
-grep "CRITICAL" system.log
+grep "CRITICAL" "$SYS_LOG_FILE"
