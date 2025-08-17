be707c7710164732dad897ed2877a211f97f0f37
root <root@LAPTOP-I3BKQONI.localdomain>
2025-08-17 17:45:42 +0600

    Using array to hold the name of error,fatal,critical

--- analyse-log.sh
+++ analyse-log.sh
@@ -4,25 +4,27 @@ LOG_DIR="/home/logs"
 APP_LOG_FILE="application.log"
 SYS_LOG_FILE="system.log"
 
+ERROR_PATTERNS=("ERROR" "FATAL" "CRITICAL")
+
 echo "analysing log files"
 echo "==================="
 echo -e "\nList of the files updated in last 24 hours" 
 find $LOG_DIR -name "*.log" -mtime -1
 
 echo -e "\nSearching Error logs in appication.log file"
-grep "ERROR" "$LOG_DIR/$APP_LOG_FILE"
+grep "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of error logs found in application.log"
-grep -c "ERROR" "$LOG_DIR/$APP_LOG_FILE"
+grep -c "${ERROR_PATTERNS[0]}" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of fatal logs found in application.log"
-grep -c "FATAL" "$LOG_DIR/$APP_LOG_FILE"
+grep -c "${ERROR_PATTERNS[1]}" "$LOG_DIR/$APP_LOG_FILE"
 
 echo -e "\nNumber of fatal logs found in system.log"
-grep -c "FATAL" "$SYS_LOG_FILE"
+grep -c "${ERROR_PATTERNS[1]}" "$SYS_LOG_FILE"
 
 echo -e "\nNumber of critical logs found in system.log"
-grep -c "CRITICAL" "$SYS_LOG_FILE"
+grep -c "${ERROR_PATTERNS[2]}" "$SYS_LOG_FILE"
 
 echo -e "\nNumber of critical logs found in system.log"
-grep "CRITICAL" "$SYS_LOG_FILE"
+grep "${ERROR_PATTERNS[2]}" "$SYS_LOG_FILE"
