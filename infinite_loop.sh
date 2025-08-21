671f6327d1ee56961093350f582f767c44fda15e
Apon Roy <aponroy700@gmail.com>
2025-08-22 01:16:06 +0600

    Practiced Infinite_loops

---
+++ infinite_loop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+var="Hello"
+
+# This is infiinite loop for a task to run longer using while loop.
+
+while true
+do
+	echo "Hi buddy"
+	sleep 2s
+done
+
+
---
+++ infinite_loop_02.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+for (( ;; ))
+do 
+	echo "Hello from Apon"
+	sleep 2s
+done
